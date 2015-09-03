0d1fd122cfc3e8dae6918b83f3de7220bb51e62e
Topher White <t@pher.me>
2015-09-03 03:00:01 -0700

    moving press assets into sub-directory

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $SCRIPT_DIR/../;
+
 git checkout gh-pages;
 git merge master -m "merging master into gh-pages";
 git push;
